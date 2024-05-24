597f0c4900a0c15f14bc9a61d5f23e012a4d8c33
Mariia Ignateva <ignateva.mariya2@wb.ru>
2024-05-24 15:57:20 +0300

    add cards

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,25 @@
 // @todo: Темплейт карточки
+function removeCard(event) {
+    const listItem = event.target.closest('.card');
+    listItem.remove();
+}
 
+function createCard(cardData, removeCardFunc) {
+    const cardTemplate = document.querySelector('#card-template').content;
+    const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+    const removeButton = cardElement.querySelector('.card__delete-button');
+    cardElement.querySelector('.card__title').textContent = cardData.name;
+    cardElement.querySelector('.card__image').src = cardData.link;
+    cardElement.querySelector('.card__image').alt = cardData.name;
+    removeButton.addEventListener('click', removeCardFunc);
+    return cardElement
+}
+
+const container = document.querySelector('.places');
+const placeContainer = container.querySelector('.places__list');
+initialCards.forEach(function (elem) {
+    placeContainer.append(createCard(elem, removeCard));
+})
 // @todo: DOM узлы
 
 // @todo: Функция создания карточки
